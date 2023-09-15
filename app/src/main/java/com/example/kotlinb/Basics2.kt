cb72cbb0edc2a4ec60f04c327f54398df0a1a545
Zain Najam <zainnajam2424@gmail.com>
2023-09-15 19:34:13 +0500

    Final Code

--- app/src/main/java/com/example/kotlinb/Basics2.kt
+++ app/src/main/java/com/example/kotlinb/Basics2.kt
@@ -1,3 +1,5 @@
+package com.example.kotlinb
+
 open class Cricketer(val name: String, val age: Int, val nationality: String)
 {
     open fun print()
@@ -6,17 +8,67 @@ open class Cricketer(val name: String, val age: Int, val nationality: String)
     }
 }
 
-interface Batsman
-{
+interface Batsman {
     var score: Int
     var batsmanRanking: Int
 
-    fun printBatsmanDetails()
-    {
+    fun printBatsmanDetails() {
         println("Score: $score, Batsman Ranking: $batsmanRanking")
     }
 
-    fun getRanking(name: String)
-    {}
+    fun retrieveBatsmanRanking(): Int
+}
+interface Bowler
+{
+    var wickets: Int
+    var bowlerRanking: Int
+
+    fun printBowlerDetails()
+    {
+        println("Wickets: $wickets, Bowler Ranking: $bowlerRanking")
+
+    }
+
+    fun retrieveBowlerRanking(): Int
 }
 
+class AllRounder(name: String, age: Int, nationality: String) : Cricketer(name, age, nationality), Batsman, Bowler{
+
+    override var score: Int = 0
+    override var batsmanRanking: Int = 0
+    override var wickets: Int = 0
+    override var bowlerRanking: Int = 0
+    var ranking: Int = 0
+
+    override fun print()
+    {
+        super.print()
+        printBatsmanDetails()
+        printBowlerDetails()
+    }
+
+    override fun retrieveBatsmanRanking(): Int
+    {
+        return batsmanRanking
+    }
+
+    override fun retrieveBowlerRanking(): Int
+    {
+        return bowlerRanking
+    }
+}
+
+fun main()
+{
+    val allRounder = AllRounder(name = "Shahid Afridi", age = 50, nationality = "Pakistan")
+    allRounder.score = 34
+    allRounder.batsmanRanking = 4
+    allRounder.bowlerRanking = 3
+    allRounder.wickets = 5
+    allRounder.ranking = 4
+
+    allRounder.print()
+
+
+
+}
