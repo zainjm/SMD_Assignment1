ad38785dc292405d4e39033da8e231b0b1c9e24d
Zain Najam <zainnajam2424@gmail.com>
2023-09-15 19:21:36 +0500

    Batsman interface

--- app/src/main/java/com/example/kotlinb/Basics2.kt
+++ app/src/main/java/com/example/kotlinb/Basics2.kt
@@ -1,8 +1,22 @@
-class Cricketer(val name: String, val age: Int, val nationality: String)
+open class Cricketer(val name: String, val age: Int, val nationality: String)
 {
-    fun print()
+    open fun print()
     {
         println("Name: $name, age: $age, Nationality: $nationality")
     }
+}
+
+interface Batsman
+{
+    var score: Int
+    var batsmanRanking: Int
+
+    fun printBatsmanDetails()
+    {
+        println("Score: $score, Batsman Ranking: $batsmanRanking")
+    }
+
+    fun getRanking(name: String)
+    {}
 }
 
